9b4d09e48b4fda0508bdaa63de1bfcb2991ccdd4
maxjenius <maxjenius@gmail.com>
2025-07-15 15:06:10 -0500

    Add row-level edit output

--- AgGrid/generated/ManifestTypes.d.ts
+++ AgGrid/generated/ManifestTypes.d.ts
@@ -5,9 +5,12 @@
 // Define IInputs and IOutputs Type. They should match with ControlManifest.
 export interface IInputs {
     EditedCellsSchema: ComponentFramework.PropertyTypes.StringProperty;
+    EditedRowsSchema: ComponentFramework.PropertyTypes.StringProperty;
     gridData: ComponentFramework.PropertyTypes.DataSet;
 }
 export interface IOutputs {
     EditedCells?: any;
+    EditedRows?: any;
     EditedCellsSchema?: string;
+    EditedRowsSchema?: string;
 }
