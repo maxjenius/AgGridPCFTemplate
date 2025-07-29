56cf0048ee5bfad849e6a27992dc2cb2fc523059
maxjenius <maxjenius@gmail.com>
2025-07-29 18:17:30 -0500

    fix(date-editor): ensure latest value returned

--- AgGrid/components/FluentDateTimeCellEditor.tsx
+++ AgGrid/components/FluentDateTimeCellEditor.tsx
@@ -1,16 +1,31 @@
-import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import React, { forwardRef, useImperativeHandle, useState, useRef, useEffect } from 'react';
 import type { ICellEditorParams } from 'ag-grid-community';
 import FluentDateTimePicker from './FluentDateTimePicker';
 
 const FluentDateTimeCellEditor = forwardRef((props: ICellEditorParams, ref) => {
   const [val, setVal] = useState<string>(props.value ?? '2025-05-04T21:35');
+  const valRef = useRef(val);
+  useEffect(() => {
+    valRef.current = val;
+  }, [val]);
 
   useImperativeHandle(ref, () => ({
-    getValue: () => val,
+    getValue: () => valRef.current,
     isPopup: () => true,
   }));
 
-  return <FluentDateTimePicker autoOpen value={val} onChange={(v) => v && setVal(v)} />;
+  return (
+    <FluentDateTimePicker
+      autoOpen
+      value={val}
+      onChange={(v) => {
+        if (v) {
+          valRef.current = v;
+          setVal(v);
+        }
+      }}
+    />
+  );
 });
 
 export default FluentDateTimeCellEditor;
